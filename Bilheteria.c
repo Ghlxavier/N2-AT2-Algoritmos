9c66f9124b4d0b85a0594a1fdeec32673c9ab5d3
Andreas Gomes Marchi <162516125+AndreasGomesMarchi@users.noreply.github.com>
2024-11-22 20:47:52 -0300

    Update Bilheteria.c
    Adição da parte do Rafa (Impressão do bilhete)

--- Bilheteria.c
+++ Bilheteria.c
@@ -4,46 +4,58 @@
 
 //Interface inicial; <- eu
 //Reserva de lugar; <- Natan
-//Impressão de bilhete; <- Rafa
+//ImpressÃ£o de bilhete; <- Rafa
 //Cancelamento; <- Gustavo
-//Finalização; <- Rafa
-//e documentação <- Gustavo
+//FinalizaÃ§Ã£o; <- Rafa
+//e documentaÃ§Ã£o <- Gustavo
 
 void Interface();
+
 void Reserva();
-void Impressao();
+
+void impressao();
+int pegarBilhete();
+void exibirBilhete();
+typedef struct {
+	int quantidadeBilhete;
+	int numeroAssento;
+	char tipoIngresso[15];
+	int incluiCombo;
+} Bilhete;
+
 void Cancelar();
 
 int main(){
-    int opc = 0;
-
-    Interface();
-    scanf("%d", &opc);
+    int opc;
 
     do{
-    switch(opc){
+    	Interface();
+    	scanf("%d", &opc);
+    	
+    	switch(opc){
 
-        case 1:
-            //Reservar lugar
-        break;
-
-        case 2:
-            //Impressão de bilhetes
-        break;
-
-        case 3:
-            //Cancelar compra
-        break;
-
-        case 4:
-            printf("Obrigado por escolher a HollowMovie!!!\n");
-            printf("Volte Sempre!");
-        break;
-
-        default:
-            printf("Opcao invalida! O programa encerrara!");
-    }
-    }while(opc <= 4 || opc != 0);
+	        case 1:
+	            //Reservar lugar
+	        break;
+	
+	        case 2:
+				impressao();
+	        break;
+	
+	        case 3:
+	            //Cancelar compra
+	        break;
+	
+	        case 4:
+	            printf("Obrigado por escolher a HollowMovie!!!\n");
+	            printf("Volte Sempre!");
+	        break;
+	
+	        default:
+	            printf("Opcao invalida! O programa encerrara!");
+	        break;
+    	}
+    }while(opc < 4 && opc != 0);
 
     return 0;
 }
@@ -59,5 +71,47 @@ void Interface(){
     printf("Impressao de bilhete - [2]\n");
     printf("Cancelar compra - [3]\n");
     printf("Sair - [4]\n");
-    printf("Escolha a opcao: ");
+    printf("Escolha a opcao: ");    
+}
+
+void impressao(){
+	Bilhete bilhetes[] = {
+		{1, 10, "Inteiro", 1},
+		{2, 15, "Meia-entrada", 0},
+		{3, 20, "Inteiro", 1}
+	};
+	int tamanho = sizeof(bilhetes) / sizeof(bilhetes[0]);		
+	int numeroEncontrado;
+
+	printf("Digite o numero do bilhete que deseja buscar: ");
+	scanf("%d", &numeroEncontrado);
+				
+	int indice = pegarBilhete(bilhetes, tamanho, numeroEncontrado);
+				
+	if (indice != -1) {
+					
+		printf("\nInformacoes do bilhete:\n");
+		exibirBilhete(bilhetes[indice]);
+					
+	} else {
+					
+		printf("\nDesculpe, bilhete nao foi achado.\n");
+					
+	}			
+}
+
+int pegarBilhete(Bilhete bilhetes[], int tamanho, int numeroEncontrado) {
+	int i;
+	for (i = 0; i < tamanho; i++) {
+		if (bilhetes[i].quantidadeBilhete == numeroEncontrado) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+void exibirBilhete(Bilhete bilhete) {
+	printf("Numero do assento: %d\n", bilhete.numeroAssento);
+	printf("Tipo de ingresso: %s\n", bilhete.tipoIngresso);
+	printf("Combo incluido: %s\n", bilhete.incluiCombo ? "Sim" : "Nao");
 }
